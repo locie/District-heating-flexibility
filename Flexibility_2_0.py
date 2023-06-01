825e17ea824ed7b2be240d1c4c564a9f68e0795d
YolanBlanchon <yolan.blanchon@grenoble-inp.fr>
2023-06-01 10:07:07 +0200

    Update Flexibility_2_0.py

--- Flexibility_2_0.py
+++ Flexibility_2_0.py
@@ -18,6 +18,7 @@ plt.plot(), plt.close()
 import numpy as np
 import os
 from functools import reduce
+from math import *
 import time
 import tracemalloc
 import warnings
@@ -35,32 +36,61 @@ def Initialization(list_power_unit):
 
 
 def Add_Unit(df_flexibility, list_power_unit):
+    df=copy.deepcopy(df_flexibility)
+    df2=pd.DataFrame()
     for i in range(len(list_power_unit)):
-        df=copy.deepcopy(df_flexibility)
-        df['power']=df.loc[:,'power']+list_power_unit[i]
+        df['power']=df_flexibility.loc[:,'power']+list_power_unit[i]
         #df=df.merge(df_flexibility, how='outer', on='power')
         #df.set_index('power', inplace=True)
         #df_flexibility.set_index('power', inplace=True)
-        print(df)
-        print(df_flexibility)
-        df2 = pd.concat([df, df_flexibility], ignore_index=True)
-        print("\n")
-        print(df2)
-        df2['combinations'] = df2.duplicated().groupby(df2.index).sum().add(df2['combinations'])
-        print(df2.drop_duplicates(subset = 'power', keep = 'last'))
-        #print(df2)
-        #liste = df2.duplicate()
-        #print(liste)
-        #df1 = df2.groupby(df2.index).get_group(3).sum()
-        #print(df1)
-        #print(df.groupby('power').sum().add(df_flexibility))
-        #df['combination'] =     df.loc[:,'combinations_x']+df.loc[:,'combinations_y']  
-        #print (df)
+        df2 = pd.concat([df, df2], ignore_index=True)
+    #df2['combinations'] = df2.duplicated().groupby(df2.index).sum().add(df2['combinations'])
+    df2['combinations'] = df2.groupby("power")["combinations"].transform("sum")
+    df2 = df2.sort_values(by=['power'])
+    #print("df2 :",df2)
+    #df2['combinations'] = df2.duplicated().groupby(df2.index).sum().add(df2['combinations'])
+    df2 = df2.drop_duplicates(subset = ['power'], keep = 'first')
+    df2=df2.sort_values(by=['power'])
+    #plt.bar(df2['power'], df2['combinations'], width=0.8, bottom=None,  align='center', data=None)
+    return df2
+
+
+def build_Op_flex(list_unit):
+    tic_start = time.time()
+    if len(list_unit)<1:
+        raise ValueError('no unit')
+    if len(list_unit)==1:
+        return Initialization(list_unit[0])
+    else:
+        df=Initialization(list_unit[0])
+        for i in range(1,len(list_unit)):
+           df=Add_Unit(df, list_unit[i])
+           #df.loc[:,"combinations"]=df.loc[:,"combinations"]/max(df.loc[:,"combinations"])
+           #plt.figure()
+           #plt.bar(df['power'], df['combinations'], width=0.8, bottom=None,  align='center', data=None)
+        tic_end = time.time()
+        print("calculation time :", tic_end - tic_start)    
+        return df
     
+def plot_flexi(f):
+    l=[]
+    #for i in range(len(f)):
+        #print(f.loc[i,])
+        #l.append(log10(f.loc[:,"combinations"][i]/min(f.loc[:,"combinations"])))
+    plt.bar(f['power'], f["combinations"], width=0.8, bottom=None,  align='center', data=None)
+    
+   
 def Sub_Unit():
     pass
 
 
-list_power_unit_1 = [0 , 3 , 7]
-init=Initialization(list_power_unit_1)
-Add_Unit(init,[3])
+
+#list_unit= [list(range(0,900,100))]*1 + [list(range(0,1300,100))]*1 + [list(range(0,1450,100))]*1
+list_unit = [[0,100,200,300,400],
+             [0,100]]
+
+
+f=build_Op_flex(list_unit)
+
+plt.plot(f['power'], f["combinations"])
+#plt.plot(f['power'], f['combinations'])
